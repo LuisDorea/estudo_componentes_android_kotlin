0a7204b5b470b9beaa33196b6595189c4b62c69f
luis_dorea <luis.dorea@outlook.com>
2019-06-23 12:24:37 -0300

    Validações de campos

--- ExemploEditText/app/src/main/java/br/com/exemploedittext/MainActivity.kt
+++ ExemploEditText/app/src/main/java/br/com/exemploedittext/MainActivity.kt
@@ -1,27 +1,37 @@
 package br.com.exemploedittext
 
-import android.os.Build.VERSION_CODES.O
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.AppCompatEditText
-import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity :
-    AppCompatActivity(),
-    View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bt_entrar_id.setOnClickListener(this)
-
-
+        bt_entrar_id.setOnClickListener {
+            if (validarCampos()){
+                Toast.makeText(this, "O e-mail ${et_email_id.text} está logado!", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
-    override fun onClick(v: View?) {
-        Toast.makeText(this, "O e-mail ${et_email_id.text} está logado!", Toast.LENGTH_LONG).show()
+    fun validarCampos(): Boolean{
+
+        var tudoOk = true
+
+        if (et_email_id.text.toString().isEmpty()){
+            et_email_id.setError("Campo Obrigatório!")
+            tudoOk = false
+        }
+
+        if (et_senha_id.text.toString().isEmpty()){
+            et_senha_id.setError("Campo Obrigatório!")
+            tudoOk = false
+        }
+
+        return tudoOk
     }
 }
